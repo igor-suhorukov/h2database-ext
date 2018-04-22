ab7eb7f331b2c1ddb32ea9c65199d9a8ef7c20e9
igor-suhorukov <igor.suhorukov@gmail.com>
2018-04-22 21:46:25 +0300

    initial commit

---
+++ src/main/java/com/github/igorsuhorukov/h2/XQuery3.java
@@ -0,0 +1,47 @@
+package com.github.igorsuhorukov.h2;
+
+import org.basex.core.Context;
+import org.basex.query.QueryException;
+import org.basex.query.QueryProcessor;
+import org.basex.query.value.Value;
+import org.basex.query.value.item.Item;
+import org.h2.tools.Csv;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.ResultSet;
+import java.util.Iterator;
+import java.util.Map;
+
+public class XQuery3 {
+
+    public static ResultSet query(String xquery) throws QueryException, IOException {
+        return queryWithParameters(xquery, null);
+    }
+
+    public static ResultSet queryWithParameters(String xquery, Map<String, Object> parameters) throws QueryException, IOException {
+        try (QueryProcessor queryProcessor = new QueryProcessor(xquery, new Context())) {
+            bindParameters(queryProcessor, parameters);
+            Value queryResult = queryProcessor.value();
+            Iterator<Item> resultIterator = queryResult.iterator();
+            if(!resultIterator.hasNext()){
+                throw new IllegalArgumentException("Query result must be csv values with headers");
+            }
+            Item xqResultSet = resultIterator.next();
+            String csvResult = Item.toString(xqResultSet.string(null), false, false);
+            return new Csv().read(new StringReader(csvResult), null);
+        }
+    }
+
+    private static void bindParameters(QueryProcessor queryProcessor, Map<String, Object> parameters) {
+        if(parameters!=null && !parameters.isEmpty()) {
+            parameters.forEach((name, parameter) -> {
+                try {
+                    queryProcessor.bind(name, parameter);
+                } catch (QueryException e) {
+                    throw new IllegalArgumentException(name, e);
+                }
+            });
+        }
+    }
+}
